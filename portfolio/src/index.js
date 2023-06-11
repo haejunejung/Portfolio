135563d4d8a1268ca49ad3e925186203ccbd56d5
haejunejung <jhj9422@unist.ac.kr>
2023-06-11 17:01:31 +0900

    portfolio

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import TodayILearned from "./components/TIL/TodayILearned";
 import Projects from "./components/Project/Project";
@@ -9,11 +9,18 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" exact={true} element={<Home />} />
-        <Route path="/TIL" element={<TodayILearned />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <section>
+        <Link to="/">홈</Link>
+        <Link to="/TIL">하루하루의 기록들</Link>
+        <Link to="/projects">프로젝트 소개</Link>
+      </section>
+      <section>
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/TIL" element={<TodayILearned />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </section>
     </BrowserRouter>
   </React.StrictMode>
 );
