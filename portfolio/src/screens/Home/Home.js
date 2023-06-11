f8a19281a2d621bdf1b98bc10a33267b34bfb630
haejunejung <jhj9422@unist.ac.kr>
2023-06-11 17:40:43 +0900

    portfolio

--- portfolio/src/screens/Home/Home.js
+++ portfolio/src/screens/Home/Home.js
@@ -34,14 +34,25 @@ const Home = () => {
                 <div className="value">Phone: +82 10-2009-9422</div>
                 <div className="value">E-mail: gowns0408@gmail.com</div>
                 <div className="value">
-                  Github: https://github.com/haejunejung
+                  Github:{" "}
+                  <Link to="https://github.com/haejunejung">
+                    https://github.com/haejunejung
+                  </Link>
                 </div>
-                <div className="value">Blog: https://haejunejung.github.io</div>
                 <div className="value">
-                  TIL: <Link to="/TIL">TIL</Link>
+                  Blog:{" "}
+                  <Link to="https://haejunejung.github.io">
+                    https://haejunejung.github.io
+                  </Link>
                 </div>
                 <div className="value">
-                  Portfolio: <Link to="/Projects">Projects</Link>
+                  TIL:{" "}
+                  <Link to="https://leather-river-49e.notion.site/7a97411a3b8b49bd82dd5bd7103efc93?v=bc7f75c189e949bc9ba99e9e5782ccbe&pvs=4">
+                    하루하루의 기록들
+                  </Link>
+                </div>
+                <div className="value">
+                  Projects: <Link to="/Projects">개인&팀 프로젝트</Link>
                 </div>
               </div>
             </div>
