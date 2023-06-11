ba080423869b5dd7ec8302c2f9c726261edc2302
haejunejung <jhj9422@unist.ac.kr>
2023-06-11 16:56:25 +0900

    portfolio

--- portfolio/src/components/Projects/Projects.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Projects = () => {
-  return <div>Projects 준비중</div>;
-};
-
-export default Projects;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import TodayILearned from "./components/TIL/TodayILearned";
+import Projects from "./components/Project/Project";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/TIL" element={<TodayILearned />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
