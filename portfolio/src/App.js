0ac069f448580d36abb2a6aaa7b06f76b57fa437
haejunejung <jhj9422@unist.ac.kr>
2023-06-11 12:56:24 +0900

    portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,23 +6,44 @@ function App() {
   return (
     <React.Fragment>
       <section className="card">
-        <div style={{ float: "left" }}>
-          <img className="my-picture__" alt="it's me" src={ME} />
-        </div>
-        <div className="explain_container__">
-          <header className="title">
-            <div>정해준 (Haejune, Jung)</div>
-            <div style={{ color: "#37B9F1" }}>미래의 개발자</div>
-          </header>
-          <main className="explain_me__">
-            <div>Phone: +82 10-2009-9422</div>
-            <div>E-mail: gowns0408@gmail.com</div>
-            <div>Github: https://github.com/haejunejung</div>
-            <div>TIL: https://haejunejung.github.io/TIL</div>
-            <div>Blog: https://github.com/haejunejung.github.io</div>
-            <div>AboutMe: https://haejunejung.github.io/AboutMe</div>
-            <div>Portfolio: https://haejunejung.github.io/Portfolio</div>
-          </main>
+        <div id="card-front" className="card-front" style={{ display: "flex" }}>
+          <div className="card-avatar">
+            <img
+              alt="it's me"
+              src={ME}
+              style={{
+                marginLeft: "1rem",
+                marginRight: "1rem",
+                width: "15rem",
+                height: "15rem",
+              }}
+            />
+          </div>
+          <div className="card-details">
+            <div id="name" className="name">
+              정해준 (Haejune, Jung)
+            </div>
+            <div className="occupation">미래의 개발자</div>
+            <div className="info">
+              <div className="value">Phone: +82 10-2009-9422</div>
+              <div className="value">E-mail: gowns0408@gmail.com</div>
+              <div className="value">
+                Github: https://github.com/haejunejung
+              </div>
+              <div className="value">
+                TIL: https://haejunejung.github.io/TIL
+              </div>
+              <div className="value">
+                Blog: https://github.com/haejunejung.github.io
+              </div>
+              <div className="value">
+                AboutMe: https://haejunejung.github.io/AboutMe
+              </div>
+              <div className="value">
+                Portfolio: https://haejunejung.github.io/Portfolio
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </React.Fragment>
